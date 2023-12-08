9444aff84ce56d986ac1c26a9be1bae56c900699
unknown <waliuwaheed2021@gmail.com>
2023-12-08 11:19:36 -0800

    update

--- pages/qiuz.vue
+++ pages/qiuz.vue
@@ -6,11 +6,13 @@
         <div class="">
           <div class=" sm:h-32 h-fit py-3 px-4 text-white text-xl  bg-primary w-full">
             <h1 class=" sm:text-xl text-[14px] leading-tight mb-10">  Notice: {{ questions[questionIndex].notice }} </h1>
-            <h1 class="font-semibold text-xl"> {{ questions[questionIndex].question }} </h1>
+            <h1 class="font-semibold text-xl"> {{ selectedQuestions[questionIndex].question }} </h1>
           </div>
           <div class=" py-3 px-5">
-
-            <label class="containerss" v-for="(option, index) in questions[questionIndex].choices" :key="index">
+            <p>Time left: {{ Math.floor(timeLeft / 60) }} minutes {{ timeLeft % 60 }} seconds</p>
+    <!-- Add a start button -->
+    <button @click="startTimer">Start</button>
+            <label class="containerss" v-for="(option, index) in selectedQuestions[questionIndex].choices" :key="index">
               {{ option }}
               <input type="radio" v-model="this.selectedOption" :value="option"  name="radio">
               <span class="checkmark"></span>
@@ -21,18 +23,21 @@
 
         </div>
         <div class=" flex px-4 justify-between ">
-          <button @click="previous" class=" py-2 px-4 t text-white bg-primary hover:bg-green-800 ease-in-out  duration-500">
+          <button  @click="previous" class=" py-2 px-4 t text-white bg-primary hover:bg-green-800 ease-in-out  duration-500">
             Previous 
           </button>
-          <button @click="next" class=" py-2 px-4 t text-white bg-primary hover:bg-green-800 ease-in-out  duration-500">
+          
+          <button v-if="questionIndex < questions.length - 1" @click="next" class=" py-2 px-4 t text-white bg-primary hover:bg-green-800 ease-in-out  duration-500">
           Next
-
+          </button>
+          <button v-else @click="submite" class=" py-2 px-4 t text-white bg-secondary hover:bg-red-600 ease-in-out  duration-500">
+            Submit 
           </button>
         </div>
 
         <div class="mt-7 py-3 px-5 flex justify-center w-full gap-3">
           <div class="sm:py-3 border-black  border-2 sm:px-5 py-1 px-3 rounded-full w-fit h-fit font-semibold  text-black"
-            v-for="(q, index) in questions" :key="index" @click="goToQuestion(index)" :class="{
+            v-for="(q, index) in selectedQuestions" :key="index" @click="goToQuestion(index)" :class="{
                'bg-red text-primary border-primary bg-white': questionIndex === index && q.userAnswer === null,
       'bg-primary border-primary text-white': questionIndex !== index && q.userAnswer !== null,
       'bg-white border-primary': q.userAnswer === null,
@@ -42,20 +47,30 @@
           </div>
         </div>
       </section>
-      
 
-       <div :class=" submiteEnter ? 'w-full h-full'  : 'w-0 h-0 '"   class="  : bg-opacity-75   flex justify-center items-center    py-1 fixed   px-4 ">
+       <div :class=" submiteEnter ? 'w-0 h-0'  : 'w-full h-full '"   class=" hidden  w-full : bg-opacity-75   flex justify-center items-center    py-1 fixed   px-4 ">
 
         <div :class=" submiteEnter ? 'translate-y-0' : 'translate-y-[1000px]'"  class="duration-700  bg-white shadows py-2 px-2 relative sm:w-1/4 w-full ">
           <h1 class=" text-xl font-semibold text-center" > HMMMMM <br>ARE YOU READY TO SUBMITE</h1>
         <div class="flex gap-3 pt-3 text-white ">
-        <button @click="unsubmite()" class="text-2xl py-2  font-medium bg-secondary w-full  ">No</button>
+
+        <button   @click="unsubmite()" class="text-2xl py-2  font-medium bg-secondary w-full  ">No</button>
+       
         <button @click="submite()" class="text-2xl py-2 font-medium bg-primary w-full  ">Yes</button>
 
         </div>
         
         </div>
-              </div>
+        </div>
+
+        <div>
+          <div>
+          <div  class="duration-700 shadows py-2 z-20  fixed bg-black w-full ">
+              <h1  class="text-3xl text-semibold w-full">You Win</h1>
+
+          </div>
+        </div>
+      </div>
     </div>
 
 
@@ -69,6 +84,27 @@
 const questions = [
   {
     notice: 'Pick one that compudatle to this question ',
+    question: "What is American football called in England?",
+    choices: ["American football", "football", "Handball"],
+    rightAnswer: "American football",
+    userAnswer: null, // This will hold the user's answer
+  },
+  {
+    notice: '2Pick one that compudatle to this question ',
+    question: "What is American football called in England?",
+    choices: ["American football", "football", "Handball"],
+    rightAnswer: "American football",
+    userAnswer: null, // This will hold the user's answer
+  },
+  {
+    notice: '3Pick one that compudatle to this question ',
+    question: "What is American football called in England?",
+    choices: ["American football", "football", "Handball"],
+    rightAnswer: "American football",
+    userAnswer: null, // This will hold the user's answer
+  },
+  {
+    notice: '4Pick one that compudatle to this question ',
     question: "What is American football called in England?",
     choices: ["American football", "football", "Handball"],
     rightAnswer: "American football",
@@ -100,9 +136,40 @@ export default {
       questions,
       questionIndex: 0,
       score: 0,
+      timer: null,
+      timeLeft: 300,
+      selectedQuestions: [],
     };
   },
   methods: {
+    selectQuestions() {
+    let indices = Array.from({length: this.questions.length}, (_, i) => i); // Array of indices
+    indices.sort(() => Math.random() - 0.5); // Shuffle the indices
+    this.selectedQuestions = indices.slice(0, 5).map(i => this.questions[i]); // Select the first 5
+  },
+    startTimer() {
+      // Set the timer for 5 minutes (300000 milliseconds)
+      
+    // Clear any existing timer
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timeLeft = 300; // Reset the time
+    }
+
+    // Set the timer for 5 minutes (300 seconds)
+    this.timer = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.submite();
+      }
+    }, 1000);
+
+    },
+    
+    preSubmite(){
+      this.submiteEnter = true
+    },
     unsubmite(){
 
     },
@@ -125,6 +192,9 @@ export default {
   } else {
     this.submiteEnter = true
     console.log('Questions have finished');
+    clearTimeout(this.timer);
+      this.startTimer();clearTimeout(this.timer);
+      this.startTimer();
     // this.submite();
   }
 },
@@ -141,6 +211,7 @@ export default {
       }
     },
     submite() {
+      clearTimeout(this.timer)
       let score = 0;
       for (let question of this.questions) {
         if (question.userAnswer === question.rightAnswer) {
@@ -159,7 +230,11 @@ export default {
       console.log(`User picked: ${newVal}`);
     }
 
-  }
+  },
+  created() {
+    this.selectQuestions();
+    // Start the timer when the component is created
+  },
 };
 
 </script>
@@ -220,7 +295,7 @@ export default {
 
 /* When the radio button is checked, add a blue background */
 .containerss input:checked ~ .checkmark {
-  background-color: #2196F3;
+  background-color: #164b3b;
 }
 
 /* Create the indicator (the dot/circle - hidden when not checked) */
--- pages/test.vue
+++ pages/test.vue
@@ -8,25 +8,6 @@
     </label>
     <button @click="next()">next</button> <button @click="previous()" >previous</button> <button @click="submite()" >submite</button>
   </div>
-  
-    <!-- ... -->
-    
-<!-- <label class="container">One
-  <input type="radio" checked="checked" name="radio">
-  <span class="checkmark"></span>
-</label>
-<label class="container">Two
-  <input type="radio" name="radio">
-  <span class="checkmark"></span>
-</label>
-<label class="container">Three
-  <input type="radio" name="radio">
-  <span class="checkmark"></span>
-</label>
-<label class="container">Four
-  <input type="radio" name="radio">
-  <span class="checkmark"></span>
-</label> -->
   </template>
   
   <script>
